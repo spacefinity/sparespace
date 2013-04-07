e82eea973013612069ad356563987555da407511
Joe Esposito <joe@joeyespo.com>
2013-04-06 21:09:22 -0400

    Move map code to JS file.

--- js/list.js
+++ js/list.js
@@ -1,17 +1,29 @@
 $(function() {
-  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-  var mapOptions = {
-    zoom: 4,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+  var stockholm = new google.maps.LatLng(59.32522, 18.07002);
+  var parliament = new google.maps.LatLng(59.327383, 18.06747);
+  var marker;
+  var map;
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
 
   var canvas = $('#map-canvas');
-  var map = new google.maps.Map(canvas.get(0), mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title:"Hello World!"
+  map = new google.maps.Map(canvas.get(0), {
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: stockholm
   });
+
+  marker = new google.maps.Marker({
+    map:map,
+    draggable:true,
+    animation: google.maps.Animation.DROP,
+    position: parliament
+  });
+  google.maps.event.addListener(marker, 'click', toggleBounce);
 });
