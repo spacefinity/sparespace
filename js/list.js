204103bf16b648647cffc58105f8f769c69205ef
Joe Esposito <joe@joeyespo.com>
2013-04-07 12:30:18 -0400

    Add basic listing template and knockout template.

--- js/list.js
+++ js/list.js
@@ -1,5 +1,15 @@
 $(function() {
   init_maps();
+
+  var vm = {
+    listings: [
+      { title: 'Dry Basement for Rent', verified: true, positive_reviews: 26, negative_reviews: 0, cost: 20, photo_url: "img/space-01.png" },
+      { title: 'Spare Bedroom for Rent', verified: false, positive_reviews: 11, negative_reviews: 1, cost: 12, photo_url: "img/space-02.png" },
+      { title: 'Large Spacious Attic', verified: true, positive_reviews: 2, negative_reviews: 0, cost: 15, photo_url: "img/space-03.png" },
+      { title: 'Basement Nook', verified: true, positive_reviews: 8, negative_reviews: 0, cost: 25, photo_url: "img/space-04.png" }
+    ]
+  };
+  ko.applyBindings(vm);
 });
 
 function init_maps() {
